d8cb411df2aa69b470831ca11e954086ea09ddcb
Hemant <hemant@tecvololabs.com>
2023-07-22 01:08:51 +0530

    Added

--- src/common/components/Card.jsx
+++ src/common/components/Card.jsx
@@ -3,12 +3,12 @@ import { VideoData } from '../../app/data/YouTubeData'
 
 const Card = () => {
   return (
-    <div className='mt-24 mx-10 mb-0 grid grid-cols-3'>
+    <div className='mt-24 mx-10 mb-0 grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1'>
         {VideoData.map((item)=>{
             // var viewCount = item.videoViews;
             // if(viewCount>=1&&viewCount<1000)viewCount=viewCount;
-            // if(viewCount>=1000&&viewCount<100000)viewCount=viewCount+"K";
-            // if(viewCount>=100000&&viewCount<100000000)viewCount=viewCount+"M";
+            // if(viewCount>=1000&&viewCount<100000)viewCount=(viewCount/1000).toFixed(1)+"K";
+            // if(viewCount>=100000&&viewCount<10000000)viewCount=(viewCount/100000).toFixed(1)+"M";
             // console.log(viewCount);
             return(
                 <div className='p-2'>
@@ -19,10 +19,10 @@ const Card = () => {
                         <img className='w-8 h-8 rounded-full' src={item.channelImage} alt='channel-image'/>
                         <div>
                             <p className='text-[0.9rem] font-semibold' title={item.videoTitle}>
-                                {item.videoTitle.length >=75?item.videoTitle.substr(0,75)+"...":item.videoTitle}
+                                {item.videoTitle.length >=65?item.videoTitle.substr(0,65)+"...":item.videoTitle}
                             </p>
                             <p className='text-xs m-0'>{item.channelName}</p>
-                            <p className='text-xs m-0'>{item.channelName}</p>
+                            <p className='text-xs m-0'>{}</p>
                         </div>
                     </div>
                 </div>
